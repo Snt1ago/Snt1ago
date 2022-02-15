328e7e4cd789166073cc6e0d0c931366c3ebb9a3
Snt1ago <santiagoicasuriaga4@gmail.com>
2022-02-15 11:33:40 -0300

    subiendo archivos

---
+++ src/ciclos/Bucles.java
@@ -0,0 +1,18 @@
+package ciclos;
+
+public class Bucles {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1;
+		
+		while (1 < 10) {
+			System.out.print("fhfhfhfh: " + i);
+		i+=2;
+		
+		}
+		
+		
+	}
+
+}
